2673c631b48c7915a4eb28741260dc5afa09a48e
Thirdy Gayares <gayaresthird@gmail.com>
2025-07-09 14:28:06 +0800

    fix: add missing comma in CORS_ALLOWED_ORIGINS

--- app/core/cors.py
+++ app/core/cors.py
@@ -3,6 +3,6 @@ from app.core.settings import settings
 
 CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000",
-        "https://smartreports.vercel.app"
+        "https://smartreports.vercel.app",
         "https://smartreports.thirdygayares.com",
 ]
